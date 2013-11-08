a428ea79f9ee700f02bbd13c6c69bc491e9d6ea2
chinhtle <cle@Chinh-Les-MacBook-Pro.local>
2013-11-08 01:58:42 -0800

    Initial commit of shopping guide
    Shopping guide MVC structure with incomplete guide.  Includes
    template/concept of MVC to get started.  Needs reworking.

---
+++ ShoppingGuide/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ShoppingGuide
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{username}/{password}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ShoppingGuide/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ShoppingGuide.Models;
+using ShoppingGuide.Helpers;
+
+namespace ShoppingGuide.Controllers
+{
+    public class AccountController : ApiController
+    {
+        AccountDbAccess accDb = new AccountDbAccess();
+
+        /* TODO: This is just temporary for testing.  We definitely won't send
+         * back username and password! Most likely will just send back 
+         * result.*/
+        [HttpGet]
+        public Account FindAccount(string username, string password)
+        {
+            return accDb.findAccount(username, password);
+        }
+    }
+}
