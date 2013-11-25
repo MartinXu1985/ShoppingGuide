79a5491ca212867169cd9bfd743004cbc4f31a43
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2013-11-25 14:27:48 -0800

    Search Functionality

---
+++ ShoppingGuide/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingGuide.Models;
+
+namespace ShoppingGuide.Controllers
+{
+    public class SearchController : Controller
+    {
+        ShoppingGuideDB db = new ShoppingGuideDB();
+        //
+        // GET: /Search/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Search/
+        [HttpPost]
+        public ActionResult SearchResult(String search)
+        {
+            //var result = dbStore.Albums.Where(g => g.Title == search);
+            var result = db.Product.SqlQuery("SELECT * FROM Products WHERE Name LIKE " + "'%" + search + "%'").ToList();
+            return View(result);
+        }
+    }
+}
